32aeff9cbedc93ccd6155b74417e7cd22b027dd3
Alex Messina <me@alexmessina.dev>
2024-02-14 18:30:07 -0600

    Fixed icons not rendering correctly

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,12 +45,11 @@ module.exports = () => {
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512], 
-            //destination: path.join('assets', 'icons')
+            destination: path.join('assets', 'icons')
           },
-          
-        ]
+        ],
+        fingerprints: false
       }),
-
     ],
 
     module: {
